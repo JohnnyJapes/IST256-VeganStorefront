1fd668463aca5b1d4b73747f34c6bb2ebef889fc
Luke Hanrahan <lhanrahan2@gmail.com>
2023-06-24 20:04:51 -0400

    Shopper Edit Page no Database

--- shopperEdit.js
+++ shopperEdit.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
     let ageInput = $("#age");
     let addressInput = $("#address");
     let phoneInput = $("#phonenum");
+    let alertPlaceholder = $("#alertPlaceholder");
 
 
     form.on("submit", submit);
@@ -34,6 +35,18 @@ $(document).ready(function () {
     ageInput.on("input", (event) => {
         validateAge();
     })
+    addressInput.on("focusout", (event) => {
+        validateAddress();
+    })
+    addressInput.on("input", (event) => {
+        validateAddress();
+    })
+    phoneInput.on("focusout", (event) => {
+        validatePhone();
+    })
+    phoneInput.on("input", (event) => {
+        validatePhone();
+    })
 
 
 
@@ -42,109 +55,153 @@ $(document).ready(function () {
 
     function submit(event) {
         //validation logic here
-        //form.addClass("was-validated")
         event.preventDefault();
-        event.stopPropagation()
-        validateEmail();
-        validateFirstName();
-        validateLastName();
-        validateAge();
+        event.stopPropagation();
+        let err = 0;
+        err += validateEmail();
+        err += validateFirstName();
+        err += validateLastName();
+        err += validateAge();
+        err += validateAddress();
+        err += validatePhone();
+        if (err > 0) return;
+
+        jsonUser = data();
+
+        let json = "";
+        for (key in jsonUser) {
+            json += `${key} : ${jsonUser[key]} <br>`
+        }
+        console.log(json)
+        appendAlert("Success! <br> Json Shopper Object: <br>" + json, "success")
+
+
 
     }
     //fucntion for email validation
     //checks for @ sign and that the input isn't empty
     function validateEmail() {
         const re = new RegExp("[a-zA-Z0-9]+@[a-zA-Z0-9]+", "gm")
-        if (!emailInput) {
+        if (!emailInput.val()) {
             emailInput.addClass("is-invalid");
             $("#email + .invalid-feedback").text("Email Address Required.")
-            return;
+            return 1;
         }
         if (!re.test(emailInput.val())) {
             emailInput.addClass("is-invalid");
             $("#email + .invalid-feedback").text("Invalid Email Address. Please enter a valid Email address. Ex: Jack@google.com")
-            return
+            return 1;
         }
         emailInput.removeClass("is-invalid");
         emailInput.addClass("is-valid");
+        return 0;
     }
     function validateFirstName() {
         const re = /^[a-zA-z]+$/gm
-        if (!fNameInput) {
+        if (!fNameInput.val()) {
             fNameInput.addClass("is-invalid");
             $("#fName + .invalid-feedback").text("Please enter a name.")
-            return;
+            return 1;
         }
         if (!re.test(fNameInput.val())) {
             fNameInput.addClass("is-invalid");
             $("#fName + .invalid-feedback").text("Please enter a valid name. Name must not contain numbers, special characters, or spaces.")
-            return;
+            return 1;
         }
         fNameInput.removeClass("is-invalid");
         fNameInput.addClass("is-valid");
+        return 0;
     }
     function validateLastName() {
         const re = /^[a-zA-z]+$/gm
-        if (!lNameInput) {
+        if (!lNameInput.val()) {
             lNameInput.addClass("is-invalid");
             $("#lName + .invalid-feedback").text("Please enter a name.")
-            return;
+            return 1;
         }
         if (!re.test(lNameInput.val())) {
             lNameInput.addClass("is-invalid");
             $("#lName + .invalid-feedback").text("Please enter a first and last name. Name must not contain numbers or special characters.")
-            return;
+            return 1;
         }
         lNameInput.removeClass("is-invalid");
         lNameInput.addClass("is-valid");
+        return 0;
     }
     function validateAge() {
-        //console.log(parseInt(ageInput.val()))
-        //console.log(ageInput.val())
         if (!parseInt(ageInput.val())) {
             ageInput.addClass("is-invalid");
-            console.log("age")
             $("#age + .invalid-feedback").text("Please enter a valid age.")
-            return;
+            return 1;
         }
         if (parseInt(ageInput.val()) < 18) {
             ageInput.addClass("is-invalid");
             console.log("age error")
             $("#age + .invalid-feedback").text("Invalid age. Please enter a number above 17.")
-            return;
+            return 1;
         }
         if (parseInt(ageInput.val()) > 100) {
             ageInput.addClass("is-invalid");
             console.log("age")
             $("#age + .invalid-feedback").text("Invalid age. Please enter a number under 110.")
-            return;
+            return 1;
         }
         ageInput.removeClass("is-invalid");
         ageInput.addClass("is-valid");
+        return 0;
 
     }
     function validateAddress() {
+        const re = /^[0-9]+\s[a-zA-z\s]+[,]\s[a-zA-z\s]+[,][a-zA-z\s]+[,]\s[0-9]{5}$/gm
+        if (!addressInput.val()) {
+            addressInput.addClass("is-invalid");
+            $("#address + .invalid-feedback").text("Please enter a valid Address.")
+            return 1;
+        }
+        if (!re.test(addressInput.val())) {
+            addressInput.addClass("is-invalid");
+            $("#address + .invalid-feedback").text("Please enter a valid Address. Format: street, city, state, zip code(5 digits)")
+            return 1;
+        }
+        addressInput.removeClass("is-invalid");
+        addressInput.addClass("is-valid");
+        return 0;
 
     }
-    function validatePhone() { }
+    function validatePhone() {
+        const re = /^[0-9]{3}[-][0-9]{3}[-][0-9]{4}$/gm
+        if (!phoneInput.val()) {
+            phoneInput.addClass("is-invalid");
+            $("#phonenum + .invalid-feedback").text("Please enter a valid Phone Number.")
+            return 1;
+        }
+        if (!re.test(phoneInput.val())) {
+            phoneInput.addClass("is-invalid");
+            $("#phonenum + .invalid-feedback").text("Please enter a valid Phone Number. Format: 123-456-7890")
+            return 1;
+        }
+        phoneInput.removeClass("is-invalid");
+        phoneInput.addClass("is-valid");
+        return 0;
+
+
+    }
+    function data() {
+        var data = {
+            "email": emailInput.val(),
+            "name": fNameInput.val() + " " + lNameInput.val(),
+            "age": ageInput.val(),
+            "address": addressInput.val(),
+            "phone": phoneInput.val()
+        }
+        return data;
+    }
+
+    function appendAlert(message, type) {
+        alertPlaceholder.html(`<div class="alert alert-${type} alert-dismissible" role="alert">` +
+            `   <div>${message}</div>` +
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>' +
+            '</div>')
+    }
 
 })
-
-// // Example starter JavaScript for disabling form submissions if there are invalid fields
-// (() => {
-//     'use strict'
-
-//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//     const forms = document.querySelectorAll('.needs-validation')
-
-//     // Loop over them and prevent submission
-//     form.on('submit', event => {
-//         if (!form.checkValidity()) {
-//             event.preventDefault()
-//             event.stopPropagation()
-//         }
-
-//         form.classList.add('was-validated')
-//     }, false)
-
-// })()
