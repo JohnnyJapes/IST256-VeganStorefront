0e8cc296e9ed4ab3e048eb0e7dbfdbe631999678
Luke Hanrahan <lhanrahan2@gmail.com>
2023-08-09 20:26:52 -0400

    typos

--- Javascript/shopperEdit.js
+++ Javascript/shopperEdit.js
@@ -86,7 +86,7 @@ $(document).ready(function () {
     let alertPlaceholder = $("#alertPlaceholder");
 
 
-    form.on("submit", submit);
+    //form.on("submit", submit);
     emailInput.on("focusout", (event) => {
         validateEmail();
     })
